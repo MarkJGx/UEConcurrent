13569bbfd09e0631e84a5f9022b28378cd6359d5
MarkJGx <mark.jg@chemicalheads.com>
2026-08-16 01:57:07 +0300

    CastToUnderlying: add static_assert message

--- Source/UEConcurrent/Public/Utilities/CastToUnderlying.h
+++ Source/UEConcurrent/Public/Utilities/CastToUnderlying.h
@@ -8,7 +8,7 @@
 template <class EnumType>
 constexpr auto CastToUnderlying(EnumType Type)
 {
-	static_assert(TIsEnum<EnumType>::Value, "");
+	static_assert(TIsEnum<EnumType>::Value, "CastToUnderlying requires an enum type!");
 	using UnderType = __underlying_type(EnumType);
 	return static_cast<UnderType>(Type);
 }
